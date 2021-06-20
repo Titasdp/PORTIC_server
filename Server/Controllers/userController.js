c07a311fe7c84267fd0ff5ce52dcdb7fccdabe79
Tiago De Pina <tiagopina20014@gmail..com>
2021-06-20 21:26:43 +0100

    Change img url

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -640,7 +640,7 @@ const fetchUserProfileById = async (dataObj) => {
                     if (el.id_picture !== null) {
                         let fetchImgResult = await pictureController.fetchPictureInSystemById(el.id_picture);
 
-                        let url = await imgConvertPack.supremeConvert(fetchImgResult.toClient.processResult);
+                        let url = await imgConvertPack.convertImage(fetchImgResult.toClient.processResult);
 
                         userObj.picture = url
                     }
