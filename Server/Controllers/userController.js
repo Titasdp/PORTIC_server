f3a3339fb27356529138202585e6ce0e65e5842d
Tiago De Pina <tiagopina20014@gmail..com>
2021-07-06 16:14:33 +0100

    changes

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -1000,7 +1000,7 @@ const updateUserLevel = async (dataObj) => {
         return processResult
     }
 
-    console.log(fetchResult.toClient.processResult);
+    console.log(fetchResult.toClient.processResult.id_user_level);
     await sequelize
         .query(
             `UPDATE User SET User.id_user_level =:id_user_level  Where User.id_user=:id_user `, {
