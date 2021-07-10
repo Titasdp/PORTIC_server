b30ea9d143014609916fbea2490e92e858cc8b42
Tiago De Pina <tiagopina20014@gmail..com>
2021-07-10 19:56:43 +0100

    change data formate

--- Server/Controllers/entityController.js
+++ Server/Controllers/entityController.js
@@ -757,10 +757,10 @@ const editEntity = async (dataObj) => {
                     youtube: (!dataObj.req.sanitize(dataObj.req.body.youtube)) ? null : dataObj.req.sanitize(dataObj.req.body.youtube),
                     twitter: (!dataObj.req.sanitize(dataObj.req.body.twitter)) ? null : dataObj.req.sanitize(dataObj.req.body.twitter),
                     // 
-                    optional_course_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_course_menu)) ? false : dataObj.req.sanitize(dataObj.req.body.optional_course_menu),
-                    optional_project_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_project_menu)) ? false : dataObj.req.sanitize(dataObj.req.body.optional_project_menu),
-                    optional_recruitment_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_recruitment_menu)) ? false : dataObj.req.sanitize(dataObj.req.body.optional_recruitment_menu),
-                    optional_media_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_media_menu)) ? false : dataObj.req.sanitize(dataObj.req.body.optional_media_menu),
+                    optional_course_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_course_menu)) ? 0 : dataObj.req.sanitize(dataObj.req.body.optional_course_menu),
+                    optional_project_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_project_menu)) ? 0 : dataObj.req.sanitize(dataObj.req.body.optional_project_menu),
+                    optional_recruitment_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_recruitment_menu)) ? 0 : dataObj.req.sanitize(dataObj.req.body.optional_recruitment_menu),
+                    optional_media_menu: (!dataObj.req.sanitize(dataObj.req.body.optional_media_menu)) ? 0 : dataObj.req.sanitize(dataObj.req.body.optional_media_menu),
                 }
             }, {
                 model: EntityModel.Entity
